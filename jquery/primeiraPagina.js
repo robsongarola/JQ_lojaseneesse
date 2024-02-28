1dad3074a605ae0d09cea86bbf2056878392a219
robsongarola <robsongarola@gmail.com>
2024-02-28 17:14:05 -0300

    a

--- jquery/primeiraPagina.js
+++ jquery/primeiraPagina.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     $('button').click(function(){
-        $('.teste').hide()
+        $('.teste').hide(2000)
     }),
     $('h4').dblclick(function(){
         $(this).hide();
     })
     $('p.red').mouseenter(function(){
         $(this).css("background-color","red")
+    }),
+    $('button.alterna1').click(function(){
+        $('p.alterna').toggle(5000);
     })
 })
