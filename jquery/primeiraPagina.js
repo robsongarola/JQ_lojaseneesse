b052eb8c839b68b933716c6eb72caecd90d5d704
robsongarola <robsongarola@gmail.com>
2024-03-11 14:35:47 -0300

    110324_1

--- jquery/primeiraPagina.js
+++ jquery/primeiraPagina.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
-    $('button').click(function(){
-        $('.teste').hide(2000)
-    }),
-    $('h4').dblclick(function(){
-        $(this).hide();
-    }),
-    $('p.red').mouseenter(function(){
-        $(this).css("background-color","red")
-    }),
-    $('button.alterna1').click(function(){
-        $('p.alterna').toggle(5000);
-    }),
-    $('button').click(function(){
-        var div = $("div.primeira");
-        div.animate({left: '100px'}, "slow");
-        div.animate({fontSize: '3em'}, "slow");     
-        });
+    $('p.des').hide()
     })    
  
 
