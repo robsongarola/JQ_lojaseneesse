8dd49a2271345c8ffae6aeaf4a530762d0208a20
robsongarola <robsongarola@gmail.com>
2024-03-27 17:18:48 -0300

    270324

--- primeiro_JQ/src/jquery.js
+++ primeiro_JQ/src/jquery.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
    $('.button1').click(function(){
       alert('O texto é :'+ $('.teste').text())
    }) 
+   $('.button2').click(function(){
+      alert('O texto digitado é :'+ $('#entrada').val())
+   })
 })
