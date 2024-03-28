00240b02dab215e71d5a2dd4e32ea17e371f2302
robsongarola <robsongarola@gmail.com>
2024-03-28 17:08:53 -0300

    270324

--- primeiro_JQ/src/jquery.js
+++ primeiro_JQ/src/jquery.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
    $('.button2').click(function(){
       alert('O texto digitado é :'+ $('#entrada').val())
    })
+   $('button.def_text1').click(function(){
+      $('p.def_text').text('Novo Texto')
+   })
+   $('button.def_html1').click(function(){
+      $('p.def_html').html('<b>Novo HTML</b>')
+   })
+   $('.cli1').mouseenter(function(){
+      $(this).val('Gabriel Garola')
+      
+   }).mouseout(function(){
+      $(this).val('Digite seu nome')
+   })
 })
