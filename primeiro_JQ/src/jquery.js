204249620588c73be4eaced63ff88a7372ac443d
robsongarola <robsongarola@gmail.com>
2024-04-10 15:32:56 -0300

    090424

---
+++ primeiro_JQ/src/jquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.button1').click(function(){
+       alert('retorna texto '+ $('.par1').text()) 
+    })
+    $('.button2').click(function(){
+        alert('retorna HTML '+ $('.par2').html())
+    })
+    $('.button3').click(function(){
+         alert('Renorna Imput '+ $('.nome').val())
+    })
+    $('.button4').click(function(){
+        alert('retorna attr : '+ $('a').attr('href'))
+    })
+    $('.button5').click(function(){
+        alert("Modificando texto : " + $('.par3').text('novo Texto'))
+    })
+})
