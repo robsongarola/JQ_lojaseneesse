1a773d8f1ca89a86547bbf0a67d606f13dc635de
robsongarola <robsongarola@gmail.com>
2024-04-17 09:31:35 -0300

    010101

--- primeiro_JQ/src/jquery.js
+++ primeiro_JQ/src/jquery.js
@@ -5,6 +5,16 @@
     var text2= $('<p></p>').text('segundo texto');
     var text3= document.createElement('p');
     text3.innerText='Terceiro texto';
-    $('body').prepend(text1, text2, text3)
+    $('body').before(text1, text2, text3)
    } 
+
+   $(document).ready(function(){
+      $('.button10').click(function(){
+         $('#pai').empty()
+      })
+      $('.button11').click(function(){
+         $('.cor').addClass('azul')
+         $('h1,h3').addClass('importante')
+      })
+   })
  
