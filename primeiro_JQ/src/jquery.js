0506e1c19f72d8fb9c10e8781c85ea8da62e7d01
robsongarola <robsongarola@gmail.com>
2024-04-29 16:38:47 -0300

    290424

--- primeiro_JQ/src/jquery.js
+++ primeiro_JQ/src/jquery.js
@@ -1,21 +1,8 @@
 
     
-   function acumula(){
-    var text1= $('p').text('primeiro texto')
-    var text2= $('<p></p>').text('segundo texto');
-    var text3= document.createElement('p');
-    text3.innerText='Terceiro texto';
-    $('body').before(text1, text2, text3)
-   } 
+ 
 
    $(document).ready(function(){
-      $('.button10').click(function(){
-         $('#pai').empty()
-      })
-      $('.button11').click(function(){
-         $('.cor').addClass('azul')
-         $('h1,h3').addClass('importante')
-      })
-      
+      $('p').css('background-color',"red")      
    })
  
